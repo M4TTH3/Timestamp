52703d918ca08b6f533373728e31548c5545ab7c
vanessamwong <vanessamwong19@gmail.com>
2024-10-10 13:35:29 -0400

    Firebase Google log-in

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+    alias(libs.plugins.compose.compiler)
 
     // Add the Google services Gradle plugin
     alias(libs.plugins.google.services)
@@ -42,12 +43,20 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
+
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.ui)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.material.icons.core)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.activity.compose.v161)
     implementation(libs.material)
     implementation(libs.play.services.maps)
     implementation(libs.androidx.constraintlayout)
@@ -55,6 +64,10 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.play.services.auth)
     implementation(libs.firebase.analytics)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.volley)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.googleid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
