8927fd7a6c0f639ecca85dddbf80b6f722a51175
vanessamwong <vanessamwong19@gmail.com>
2024-10-11 03:08:55 -0400

    Added colours :)

---
+++ mobile/src/main/java/org/timestamp/mobile/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package org.timestamp.mobile.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val bittersweet = Color(0xFFFF6F61)
+val jet = Color(0xFF2A2B2E)
+val tea_rose = Color(0xFFF7CAC9)
+val platinum = Color(0xFFE5E6EA)
+val powder_blue = Color(0xFF9CC5E1)
+val darker_bittersweet = Color(0xFFCC1A09)
