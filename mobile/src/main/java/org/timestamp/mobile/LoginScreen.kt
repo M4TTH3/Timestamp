3e9142420946d93f177ec00a43c513810d723bd5
vanessamwong <vanessamwong19@gmail.com>
2024-10-11 04:56:40 -0400

    Added login page styling

--- mobile/src/main/java/org/timestamp/mobile/LoginScreen.kt
+++ mobile/src/main/java/org/timestamp/mobile/LoginScreen.kt
@@ -1,20 +1,29 @@
 package org.timestamp.mobile
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.material.icons.automirrored.rounded.Login
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -25,9 +34,17 @@ fun LoginScreen(
     modifier: Modifier = Modifier,
     onSignInClick: () -> Unit
 ) {
-    Surface(
+    Box(
         modifier = modifier
             .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color(0xFFCC1A09),
+                        Color(0xFFFF6F61)
+                    )
+                )
+            )
     ) {
         Column(
             modifier = Modifier
@@ -35,19 +52,39 @@ fun LoginScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Icon(
-                modifier = Modifier
-                    .size(80.dp),
-                imageVector = Icons.AutoMirrored.Rounded.Login,
-                contentDescription = null
+            Text(text = "Welcome to\nTimestamp!",
+                color = Color(0xFFE5E6EA),
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 48.sp)
+            Spacer(modifier = Modifier.size(32.dp))
+            Image(
+                painter = painterResource(id = R.drawable.cs346logoteefsmaller),
+                contentDescription = null,
+                modifier = Modifier.size(250.dp)
             )
             Spacer(modifier = Modifier.size(32.dp))
-            Button(onClick = { onSignInClick() }) {
+            Button(
+                onClick = { onSignInClick() },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF2A2B2E)
+                ),
+                modifier = Modifier
+                    .padding(bottom = 15.dp)
+                    .height(50.dp)
+            ) {
                 Text(text = "Sign in with Google",
+                    color = Color(0xFFFFFFFF),
                     fontFamily = FontFamily.Monospace,
                     fontWeight = FontWeight.SemiBold,
-                    fontSize = 20.sp)
+                    fontSize = 22.sp)
             }
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(text = "On time in no time.",
+                color = Color(0xFFE5E6EA),
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 24.sp)
         }
     }
 }
