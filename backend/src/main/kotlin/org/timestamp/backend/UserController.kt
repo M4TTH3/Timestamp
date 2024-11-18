33df0fd722d3669ca0f0de8b1f1504550cfa38ed
M4TTH3 <matthewau-yeung@hotmail.com>
2024-11-18 01:56:48 -0500

    updated backend functionality. improved frontend crud operation speed

--- backend/src/main/kotlin/org/timestamp/backend/UserController.kt
+++ backend/src/main/kotlin/org/timestamp/backend/UserController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*
 import org.timestamp.backend.config.FirebaseUser
 import org.timestamp.backend.model.User
 import org.timestamp.backend.service.UserService
+import org.timestamp.backend.viewModels.LocationVm
 
 @RestController
 @RequestMapping("/users")
@@ -22,6 +23,15 @@ class UserController(
         @AuthenticationPrincipal firebaseUser: FirebaseUser
     ): ResponseEntity<User> {
         val user = userService.createUser(firebaseUser)
+        return ResponseEntity.ok(user)
+    }
+
+    @PatchMapping("/me/location")
+    fun updateLocation(
+        @AuthenticationPrincipal firebaseUser: FirebaseUser,
+        @RequestBody location: LocationVm
+    ): ResponseEntity<User> {
+        val user = userService.updateLocation(firebaseUser, location.latitude, location.longitude)
         return ResponseEntity.ok(user)
     }
 }
