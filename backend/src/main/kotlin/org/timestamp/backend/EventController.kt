308e2abd981d02fbb0db24f900b14405cbc73296
M4TTH3 <matthewau-yeung@hotmail.com>
2024-11-30 17:23:08 -0500

    added owner into hidden dto

--- backend/src/main/kotlin/org/timestamp/backend/EventController.kt
+++ backend/src/main/kotlin/org/timestamp/backend/EventController.kt
@@ -10,6 +10,7 @@ import org.timestamp.backend.model.toDTO
 import org.timestamp.backend.service.EventService
 import org.timestamp.lib.dto.EventDTO
 import org.timestamp.lib.dto.EventLinkDTO
+import org.timestamp.lib.dto.EventUserDTO
 import java.net.URI
 import java.util.*
 
@@ -20,16 +21,33 @@ class EventController(
 ) {
 
     /**
-     * Return an EventDTO with many fields hidden
+     * Return an EventDTO with many fields hidden. The owner of the event will
+     * be the only user in the list. Location information will be hidden.
      */
     fun Event.toHiddenDTO(): EventDTO {
+        // We will add only the owner into the user list.
+        val ownerUser = this.userEvents.single { this.creator == it.user!!.id }.user!!
+        val users: List<EventUserDTO> = listOf(
+            EventUserDTO(
+                id = ownerUser.id,
+                name = ownerUser.name,
+                email = ownerUser.email,
+                pfp = ownerUser.pfp,
+                timeEst = null,
+                distance = null,
+                arrived = false,
+                arrivedTime = null
+            )
+        )
+
         return EventDTO(
             name = this.name,
             description = this.description,
             latitude = this.latitude,
             longitude = this.longitude,
             address = this.address,
-            arrival = this.arrival
+            arrival = this.arrival,
+            users = users
         )
     }
 
