f5d23aa833d9f65dd3f1a0dcbf0483b0e0809910
M4TTH3 <matthewau-yeung@hotmail.com>
2024-12-25 02:36:19 -0500

    force MMAP usage

--- backend/src/main/kotlin/org/timestamp/backend/config/GraphHopperConfig.kt
+++ backend/src/main/kotlin/org/timestamp/backend/config/GraphHopperConfig.kt
@@ -1,9 +1,14 @@
 package org.timestamp.backend.config
 
 import com.graphhopper.GraphHopper
+import com.graphhopper.GraphHopperConfig
 import com.graphhopper.config.CHProfile
 import com.graphhopper.config.Profile
+import com.graphhopper.routing.ev.EncodedValue
+import com.graphhopper.storage.GHDirectory
+import com.graphhopper.storage.MMapDataAccess
 import com.graphhopper.util.GHUtility
+import com.graphhopper.util.Parameters
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
@@ -29,42 +34,62 @@ class GraphHopperConfig(
     private val path = Path("osm/$osmFile")
     private val cachePath = Path("graph-cache")
 
+    /**
+     * CommandLineRunner bean that exits the application
+     * after the GraphHopper import is done
+     */
     @Bean
-    @Order(1)
     @org.springframework.context.annotation.Profile("graphhopper-import")
     fun graphHopperPreprocessor(): CommandLineRunner {
         return CommandLineRunner {
-            println("Running GraphHopper import...")
-            createGraphHopperInstance(path, cachePath, shouldImport = true)
             println("Import complete, exiting...")
             exitProcess(0)
         }
     }
 
+    /**
+     * Dummy variable to prevent dependency injection error
+     * when running the import profile
+     * @see graphHopperPreprocessor
+     */
     @Bean
-    @Order(2) // Run after the import to ensure no errors
+    @org.springframework.context.annotation.Profile("graphhopper-import")
+    fun graphHopperPreprocessorDummy(): GraphHopper {
+        println("Running GraphHopper import...")
+        return createGraphHopperInstance(path, cachePath, shouldImport = true)
+    }
+
+    @Bean
+    @org.springframework.context.annotation.Profile("!graphhopper-import")
     fun graphHopperInstance(): GraphHopper {
         val hopper = createGraphHopperInstance(path, cachePath)
+        if (!hopper.fullyLoaded) throw IllegalStateException("GraphHopper instance not fully loaded")
         return hopper
     }
 
     companion object {
-        private val profiles = listOf(
+        private val profilesCustom = listOf(
             Profile("car").apply {
                 customModel = GHUtility.loadCustomModelFromJar("car.json")
             },
             Profile("bike").apply {
+                // Factor elevation into the bike profile
                 customModel = GHUtility.loadCustomModelFromJar("bike.json")
             },
             Profile("foot").apply {
+                // Factor elevation into the foot profile
                 customModel = GHUtility.loadCustomModelFromJar("foot.json")
             }
         )
 
-        private val chProfiles = listOf(CHProfile("car"), CHProfile("bike"), CHProfile("foot"))
-        private const val ENCODED_VALUES = "car_access,car_average_speed," +
-                "bike_priority,mtb_rating,hike_rating,bike_access,roundabout,bike_average_speed," +
-                "foot_access,foot_priority,foot_average_speed"
+        private val chProfilesCustom = listOf(CHProfile("car"), CHProfile("bike"), CHProfile("foot"))
+
+        // Encoded values for the custom models
+        private val encodedValues = listOf(
+            "car_access", "car_average_speed", "bike_priority", "mtb_rating", "hike_rating",
+            "bike_access", "roundabout", "bike_average_speed", "foot_access", "foot_priority",
+            "foot_average_speed"
+        )
 
         private fun createGraphHopperInstance(
             path: Path,
@@ -77,15 +102,24 @@ class GraphHopperConfig(
         ): GraphHopper {
             if (path.exists().not()) throw IllegalArgumentException("OSM file does not exist")
 
-            val hopper = GraphHopper()
+            // This is the pre-setup configurations for the GraphHopper instance.
+            // Will use the fields to that of the config YML file.
+            val graphHopperConfig = GraphHopperConfig().apply {
+                // Set the data reader to the OSM file
+                putObject("datareader.file", path.absolute().toString())
+                putObject("graph.location", cachePath.absolute().toString())
 
-            // Configs for the graphhopper instance
-            hopper.osmFile = path.absolute().toString() // set the path to the OSM file
-            hopper.setGraphHopperLocation(cachePath.absolute().toString()) // cache path
+                // Set the data access to MMAP
+                putObject("graph.dataaccess", "MMAP")
 
-            hopper.encodedValuesString = ENCODED_VALUES
-            hopper.chPreparationHandler.setCHProfiles(chProfiles) // enable speed modes
-            hopper.setProfiles(profiles)
+                setProfiles(profilesCustom)
+                setCHProfiles(chProfilesCustom) // Enable CH "speed mode" - Alternatively use LM
+
+                putObject("graph.encoded_values", encodedValues.joinToString(","))
+                putObject("import.osm.ignored_highways", "")
+            }
+
+            val hopper = GraphHopper().init(graphHopperConfig)
 
             // Load graphhopper cache if it exists, otherwise create it
             if (shouldImport) hopper.importOrLoad() else hopper.load()
